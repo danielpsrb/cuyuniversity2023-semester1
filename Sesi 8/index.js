048b9f6af988c095b0f0a23f19f2582b1a0f6f3f
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2023-06-16 07:26:30 -0700

    Add files via upload

---
+++ Sesi 8/index.js
@@ -0,0 +1,17 @@
+let passwordLength  = document.getElementById("passwordLength")
+let password = document.getElementById("password")
+
+function getPassword() {
+    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numeric = "0123456789"
+    const symbol = "!@#$%^&*()_+=-{}[]';:/?.,<>~`"
+
+    const data = lowerAlphabet + upperAlphabet + numeric + symbol
+    let generator;
+    
+}
+
+function savePassword() {
+
+}
