5fd132f7150f5aed852222efb8e6b60bf10a0cce
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2023-06-16 07:23:20 -0700

    Add files via upload

---
+++ Sesi 1/data.js
@@ -0,0 +1,33 @@
+const nama = "daniel pasaribu";
+let usia = 15;
+
+let biodata = document.getElementById('biodata');
+
+function generateBiodata() {
+    let generasi; 
+
+    if (usia > 10 && usia < 18) {
+        generasi = "generasi remaja"
+    } 
+    else if (usia > 18 && usia < 30 ) {
+        generrasi = "generasi dewasa";
+    } 
+    else if (usia >= 30) {
+       generasi = "generasi tua";
+    }
+    else if (usia > 2 && usia < 10) {
+       generasi = "generasi anak anak";
+    }
+    else {
+        generasi = "generasi bayi"
+    }
+
+    return biodata.innerHTML = generasi;
+
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
+
